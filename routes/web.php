3a81f5b84cabfea7a136808166b74368b12e908e
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-28 08:59:15 -0700

    Added Jetstream/Inertia + generate Post scaffold

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('post', PostController::class);
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia\Inertia::render('Dashboard');
+})->name('dashboard');
