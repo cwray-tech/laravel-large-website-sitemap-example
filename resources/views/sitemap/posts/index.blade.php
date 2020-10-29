d96200065c757340397e53b2686ac9a6a2f0ff54
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-28 17:04:43 -0700

    Built sitemap controllers, views and routes

--- resources/views/sitemap/posts/index.blade.php
+++ resources/views/sitemap/posts/index.blade.php
@@ -1,10 +1,9 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @if($post != null)
+    @foreach($alphas as $alpha)
         <sitemap>
-            <loc>{{ route('sitemap.posts.index') }}</loc>
-            <lastmod>{{ $post->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <loc>{{ route('sitemap.posts.show', $alpha) }}</loc>
         </sitemap>
-    @endif
+    @endforeach
 </sitemapindex>
